d0480409a6025a323f81de54e04625be098eb228
Alyssondos1 <21851266-Alyssondos1@users.noreply.replit.com>
2023-08-23 13:28:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+age = int(input())
+month = 0
+year = 0
+
+while(age != 0):
+  while age >= 365:
+    age -= 365
+    year += 1
+  while age >= 30:
+    age -= 30
+    month +=1
+    
+  days = age
+  age = 0
+
+print(f"""{year} ano(s)
+{month} mes(es)
+{days} dia(s)""")
